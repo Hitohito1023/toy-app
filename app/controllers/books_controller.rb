c2c1039041c72ae54b3c641991b3d23bea248cc8
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-01-20 11:54:29 +0900

    [add]book_model

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "変更を保存しました。"
+    else
+      render 'edit', alert: "保存に失敗しました。"
+    end
+  end
+
+  def destory
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "bookを削除しました。"
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :price)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to user_path(current_user), alert:"権限がございません"
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_101138) do
+ActiveRecord::Schema.define(version: 2021_01_20_020703) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
