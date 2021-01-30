6d790195f269ab2336f4f055bd92f35e9abb52fe
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-01-30 17:27:33 +0900

    [add]evaluationカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_062216) do
+ActiveRecord::Schema.define(version: 2021_01_30_082153) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_062216) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "evaluation"
   end
 
   create_table "chats", force: :cascade do |t|
